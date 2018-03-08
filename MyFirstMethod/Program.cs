67f58906ffca66a62f03f117559eb6cf677dcd75
FiammaV <ines.perdigao.nunes@gmail.com>
2018-03-08 10:38:03 +0000

    Corrigir erro na linha 10

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace MyAula04 {
     class MyClass {
         static void CountTo10() {
-            for (int i = 1; i <= 1; i++)
+            for (int i = 1; i <= 10; i++)
                 Console.WriteLine(i);
         }
 
