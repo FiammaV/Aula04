bbde301f33b2bbd60bf843e70a2a24c35db40be6
FiammaV <ines.perdigao.nunes@gmail.com>
2018-03-08 12:12:30 +0000

    Adicionar comentários

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -6,11 +6,20 @@ using System.Threading.Tasks;
 
 namespace MyAula04 {
     class MyClass {
+        /// <summary>
+        /// It'll print the numbers the user input
+        /// </summary>
+        /// <param name="n">The number</param>
         static void CountToN(int n) {
             for (int i = 1; i <= n; i++)
                 Console.WriteLine(i);
         }
 
+        /// <summary>
+        /// My Main method, it'll ask the user for two numbers, and then
+        /// print them on the screen
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args) {
             string aux = Console.ReadLine();
             string aox = Console.ReadLine();
